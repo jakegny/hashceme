7469af88c78d95f0a270f462e95ba239e1851eb3
Jake Nylund <jakegny@gmail.com>
2017-01-04 07:30:00 -0800

    First Commit

---
+++ ch1.hs
@@ -0,0 +1,13 @@
+module Main where
+import System.Environment
+
+main :: IO ()
+main = do
+  -- -- Ch1 Exercise 2
+  -- args <- getArgs
+  -- putStrLn ("Hello, " ++ show (read (args !! 0) + read (args !! 1)))
+
+  -- -- Ch1 Exercise 3
+  -- putStrLn ("Hello. What is your name?")
+  -- name <- getLine
+  -- putStrLn ("Hello, " ++ name)
---
+++ main.hs
@@ -0,0 +1,19 @@
+module Main where
+import System.Environment
+import Text.ParserCombinators.Parsec hiding (spaces)
+
+main :: IO ()
+main = do
+  (expr:_) <- getArgs
+  putStrLn (readExpr expr)
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+readExpr :: String -> String
+readExpr input = case parse (spaces >> symbol) "lisp" input of
+  Left err -> "No Match: " ++ show err
+  Right val -> "Found value"
+
+spaces :: Parser ()
+spaces = skipMany1 space
